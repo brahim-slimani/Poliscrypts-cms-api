f540217eb28985d2ba308ff98e24e42a56afb8b7
Slimani-Ibrahim <slimani.ibr@gmail.com>
2022-02-16 19:57:34 +0100

    Add project structure

---
+++ src/main/java/com/poliscrypts/api/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.poliscrypts.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public ResponseEntity<?> main() {
+        return new ResponseEntity<>("Welcome to CMS API", HttpStatus.OK);
+    }
+}
