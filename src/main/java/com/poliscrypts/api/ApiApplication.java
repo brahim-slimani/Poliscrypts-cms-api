51ee7a2edbe899e887dd0755da4f637a3e239031
Slimani-Ibrahim <slimani.ibr@gmail.com>
2022-02-17 11:04:40 +0100

    Initialize db tables with few rows

--- src/main/java/com/poliscrypts/api/ApiApplication.java
+++ src/main/java/com/poliscrypts/api/ApiApplication.java
@@ -1,13 +1,45 @@
 package com.poliscrypts.api;
 
+import com.poliscrypts.api.entity.Contact;
+import com.poliscrypts.api.entity.Entreprise;
+import com.poliscrypts.api.repository.ContactRepository;
+import com.poliscrypts.api.repository.EntrepriseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Arrays;
 
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
 
+    @Autowired
+    ContactRepository contactRepository;
+
+    @Autowired
+    EntrepriseRepository entrepriseRepository;
+
+    /**
+     * Initailize CONTACT & ENTREPRISE Tables with few rows in runnable thread
+     *
+     * @param args
+     */
+    @Override
+    public void run(String... args) {
+        Entreprise entreprise0 = Entreprise.builder().address("PoliScrypts").tvaNumber(Long.valueOf(4349023)).build();
+        Entreprise entreprise1 = Entreprise.builder().address("Ooredoo Algérie").tvaNumber(Long.valueOf(9054054)).build();
+        entrepriseRepository.saveAll(Arrays.asList(entreprise0, entreprise1));
+
+        Contact contact0 = Contact.builder().firstName("Brahim").lastName("SLIMANI").address("Algiers").build();
+        Contact contact1 = Contact.builder().firstName("Ahmed").lastName("Amine").address("Oran").build();
+        Contact contact2 = Contact.builder().firstName("Mohamed").lastName("Ali").address("Algiers").build();
+
+        contact0.setEntreprises(Arrays.asList(entreprise0, entreprise1));
+        contact1.setEntreprises(Arrays.asList(entreprise0));
+        contactRepository.saveAll(Arrays.asList(contact0, contact1, contact2));
+    }
 }
