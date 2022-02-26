f3488a449108de47a46b63048b68517781f5ac9d
Slimani-Ibrahim <slimani.ibr@gmail.com>
2022-02-26 17:18:50 +0100

    Allow all http methods in cors configuraion

--- src/main/java/com/poliscrypts/api/config/WebConfig.java
+++ src/main/java/com/poliscrypts/api/config/WebConfig.java
@@ -5,9 +5,12 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @EnableCaching
 @Configuration
@@ -35,6 +38,14 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.HEAD.name());
     }
 }
